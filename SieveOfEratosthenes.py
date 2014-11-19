dcf7795283b63b6380e12d5f44bfa2d462fe5b97
Dhruv Singh <dhruvsingh.er@gmail.com>
2014-11-19 13:05:12 +0530

    Create SieveOfEratosthenes.py
    Version 1 : unoptimized

---
+++ SieveOfEratosthenes.py
@@ -0,0 +1,11 @@
+print 'Show prime numbers till: '
+x = int(raw_input())
+numlist = range(2,x+1)
+for num in numlist:
+    p = 2
+    while p < 50:
+        if (p * num) in numlist:
+            numlist.remove(p * num)
+        p = p + 1
+print numlist
+print 'Prime numbers till {0} are {1}'.format(x, len(numlist))
