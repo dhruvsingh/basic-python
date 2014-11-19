f1dd08c6a40c5205781079f47d210a4e4a8b1526
Dhruv Singh <dhruvsingh.er@gmail.com>
2014-11-19 13:07:10 +0530

    Update SieveOfEratosthenes.py
    Updated the while condition

--- SieveOfEratosthenes.py
+++ SieveOfEratosthenes.py
@@ -3,7 +3,7 @@ x = int(raw_input())
 numlist = range(2,x+1)
 for num in numlist:
     p = 2
-    while p < 50:
+    while p < numlist[-1]:
         if (p * num) in numlist:
             numlist.remove(p * num)
         p = p + 1
