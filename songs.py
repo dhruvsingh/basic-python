a9a293ae9ce464c68bd6d948855dc0df9f991255
Dhruv Singh <dhruvsingh.er@gmail.com>
2015-03-24 01:30:22 +0530

    Update songs.py

--- songs.py
+++ songs.py
@@ -1,31 +1,43 @@
-#The file downloads songs all audio songs from "http://www.linkersweddingsolutions.com/weddingsongs/list-of-wedding-songs-from-hindi-movies.html":
-#Work in progress
 from bs4 import BeautifulSoup
 
 import urllib2
-
 response = urllib2.urlopen('http://www.linkersweddingsolutions.com/weddingsongs/list-of-wedding-songs-from-hindi-movies.html')
 html = response.read()
 links = []
+rows = []
 
 soup = BeautifulSoup(html)
+
+table = soup.find('table', {'class': 'download'})
+
+for row in table.findAll('tr'):
+    matter = row.findAll('td')
+    print 'matter is', matter.prettify()
+
+"""
+rows.append(matter[0].contents)
+    links.append(matter[3])
+
+key = str(row[::1])
+    links[key] = row[::3]
 soup = soup.select('a[href^="http://www.mymp3singer.com/get.php?"]')
 for link in soup:
     links.append(str(link['href']))
 
-# work here
 response = urllib2.urlopen(links[0])
 html = response.read()
 
 print 'response is', response.info()
 file_size_dl = 0
-bufferSize = 8192
+
+
 f = open('testFile.mp3', 'wb')
 while True:
-    buffer = response.read(bufferSize)
+    buffer = response.read(8192)
     if not buffer:
         break
 
     file_size_dl += len(buffer)
     f.write(buffer)
 f.close()
+"""
